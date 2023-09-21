014f3829b5fd2bc1779bf04075c72e5e3c8cf3a1
Akash P S <116643229+ItzmeAkash@users.noreply.github.com>
2023-09-21 14:29:13 +0530

    prepare base model completed

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from diseaseClassifier import logger
 from diseaseClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from diseaseClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelPipeline
 
 
 STAGE_NAME = "Data Ingestion Stage"
@@ -12,3 +13,18 @@ try:
 except Exception as e:
     logger.exception(e)
     raise e
+
+
+
+STAGE_NAME = "PrePare Base Model stage"
+
+try:
+    logger.info(f">>>>>>>>> stage {STAGE_NAME}  started <<<<<<<<<<<<<")
+    prepare_base_model = PrepareBaseModelPipeline()
+    prepare_base_model.main()
+    logger.info(f">>>>>>>>> stage {STAGE_NAME} completed <<<<<<<<<<\n\nx=======x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+        
